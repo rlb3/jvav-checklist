b63c0a515e75924c94aa500ae90842bab9a39807
Robert Boone <robert.l.boone@gmail.com>
2012-03-25 10:55:38 -0500

    Move data into model file.

---
+++ src/avbooth_checklist/models/video.clj
@@ -0,0 +1,22 @@
+(ns avbooth-checklist.models.video)
+
+(def pre-counter (atom 0))
+(def pre-checks (atom []))
+
+(def post-counter (atom 0))
+(def post-checks (atom []))
+
+(defn add-pre-check [task]
+  (swap! pre-counter inc)
+  (swap! pre-checks conj {:id @pre-counter :task task}))
+
+(defn get-pre-checks []
+  @pre-checks)
+
+(defn add-post-check [task]
+  (swap! post-counter inc)
+  (swap! post-checks conj {:id @post-counter :task task}))
+
+(add-pre-check "Turn on the outside monitor")
+(add-pre-check "Turn on all monitors in the booth")
+(add-pre-check "Set out camera to the correct view")
--- src/avbooth_checklist/views/video.clj
+++ src/avbooth_checklist/views/video.clj
@@ -3,31 +3,21 @@
             [noir.content.getting-started])
   (:use [noir.core :only [defpage defpartial]]
         [hiccup.core :only [html]]
-        [hiccup.form-helpers]))
-
-(def pre-checks (atom []))
-
-(def post-checks (atom []))
-
-(defn add-pre-check [task]
-  (swap! pre-checks conj task))
-
-(defn add-post-check [task]
-  (swap! post-checks conj task))
-
-(add-pre-check "Turn on the outside monitor")
-(add-pre-check "Turn on all monitors in the booth")
-(add-pre-check "Set out camera to the correct view")
-
+        [hiccup.form-helpers]
+        [avbooth-checklist.models.video]))
 
 (defpartial item [t]
-  [:label.checkbox [:li (check-box t) t]])
+  [:label.checkbox [:li (check-box (apply str "pre" (:id t))) (:task t)]])
 
 (defpage [:get "/video"] []
   (common/layout
-   [:h1 [:p [:strong "Pre Service Check List"]]]
-   [:form.well
-    [:div#pre
-    [:ul.unstyled
-     (map item @pre-checks)]]]
-   [:h1 [:p "Post Server Check List"]]))
+   [:div.container-fluid
+    [:div.row-fluid
+     [:div.span10
+      [:h2 [:p [:strong "Pre Service Check List"]]]
+      [:form.well
+       [:div#pre
+        [:ul.unstyled
+         (for [t (get-pre-checks)]
+           [:label.checkbox [:li (check-box (str "pre" (:id t))) (:task t)]])]]]
+      [:h2 [:p "Post Server Check List"]]]]]))
